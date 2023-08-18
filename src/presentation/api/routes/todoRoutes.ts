00ca174b782c5088c6eb70b35ff59e735ab3ecda
Alessandro Bocci <a.bocci@doveconviene.com>
2023-08-18 23:02:21 +0200

    feat: add CRUD endpoints and validation where needed

--- src/presentation/api/routes/todoRoutes.ts
+++ src/presentation/api/routes/todoRoutes.ts
@@ -6,11 +6,46 @@ import { FastifyInstance, FastifyReply, FastifyRequest } from "fastify";
  * @param {Object} options plugin options, refer to https://www.fastify.io/docs/latest/Reference/Plugins/#plugin-options
  */
 async function todoRoutes(fastify: FastifyInstance, options: any) {
+    
+    const todoProperties = {
+        description: {
+            type: 'string',
+        },
+        completed: {
+            type: 'boolean'
+        }
+    };
+
+    const todoPostValidationSchema = { 
+        body: {
+            type: 'object',
+            required: ['description', 'completed'],
+            properties: todoProperties
+        }
+    };
+
+    const todoPutValidationSchema = {
+        body: {
+            type: 'object',
+            properties: todoProperties,
+            oneOf: [
+                { required: ['description'] },
+                { required: ['title'] }
+            ]
+        }
+    };
+
     fastify.get('/todo/:id?', async function (request: FastifyRequest, reply: FastifyReply) {
         await options.todoController.getTodo(request, reply);
     }),
-    fastify.post('/todo', async function (request: FastifyRequest, reply: FastifyReply) {
+    fastify.post('/todo', { schema: todoPostValidationSchema }, async function (request: FastifyRequest, reply: FastifyReply) {
         await options.todoController.createTodo(request, reply);
+    }),
+    fastify.put('/todo/:id', { schema: todoPutValidationSchema }, async function (request: FastifyRequest, reply: FastifyReply) {
+        await options.todoController.updateTodo(request, reply);
+    }),
+    fastify.delete('/todo/:id', async function (request: FastifyRequest, reply: FastifyReply) {
+        await options.todoController.deleteTodo(request, reply);
     })
 }
 
