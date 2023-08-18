352212ef524a9e18ae679f16129f2e2cfde8aec0
Alessandro Bocci <a.bocci@doveconviene.com>
2023-08-18 23:03:21 +0200

    feat: update repository interface

--- src/domain/repositories/ITodoRepository.ts
+++ src/domain/repositories/ITodoRepository.ts
@@ -1,9 +1,15 @@
-import Todo from "../models/Todo";
+import { ICreateTodo, ITodo } from "../models/ITodo";
 
 export default interface ITodoRepository {
-    getAll(limit: number, offset: number): Promise<Todo[]>;
-    getById(id: number): Promise<Todo | null>;
-    create(todo: Todo): Promise<Todo>;
-    updateById(id: number): Promise<Todo>;
-    deleteById(id: number): Promise<Todo>;
+    getAll(limit: number, offset: number): Promise<ITodo[]>;
+    
+    getById(id: number): Promise<ITodo | null>;
+    
+    getByDescription(description: string): Promise<ITodo | null>;
+    
+    create(todo: ICreateTodo): Promise<ITodo>;
+    
+    updateById(id: number, updates: Partial<ITodo>): Promise<ITodo | null>;
+    
+    deleteById(id: number): Promise<ITodo | null>;
 }
