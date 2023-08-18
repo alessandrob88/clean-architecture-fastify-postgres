09da9fc023f1264c51967fe8ed018c70a62acc9a
Alessandro Bocci <a.bocci@doveconviene.com>
2023-08-18 23:04:29 +0200

    feat: update controller

--- src/presentation/api/controllers/TodoController.ts
+++ src/presentation/api/controllers/TodoController.ts
@@ -1,22 +1,27 @@
 import { FastifyRequest, FastifyReply } from 'fastify';
+import EntityAlreadyExistsError from '../../../domain/exceptions/EntityAlreadyExistsError';
 import NotFoundError from '../../../domain/exceptions/NotFoundError';
-import ValidationError from '../../../domain/exceptions/ValidationError';
 import { ITodoController } from '../../../interfaces/controllers/ITodoController';
-import { ITodoGetParams } from '../../../interfaces/presentation/ITodoRoutesRequest';
+import { ITodoRouteParams, ITodoQueryParams } from '../../../interfaces/presentation/ITodoRoutesRequest';
 import ICreateTodoUseCase from '../../../interfaces/useCases/ICreateTodoUseCase';
+import IDeleteTodoUseCase from '../../../interfaces/useCases/IDeleteTodoUseCase';
 import IGetAllTodosUseCase from '../../../interfaces/useCases/IGetAllTodosUseCase';
 import IGetTodoUseCase from '../../../interfaces/useCases/IGetTodoUseCase';
+import IUpdateTodoUseCase from '../../../interfaces/useCases/IUpdateTodoUseCase';
 
 export class TodoController implements ITodoController {
   constructor(
     private readonly createTodoUseCase: ICreateTodoUseCase,
     private readonly getAllTodosUseCase: IGetAllTodosUseCase,
     private readonly getTodoUseCase: IGetTodoUseCase,
+    private readonly updateTodoUseCase: IUpdateTodoUseCase,
+    private readonly deleteTodoUseCase: IDeleteTodoUseCase,
   ) {}
 
   async getTodo(request: FastifyRequest, reply: FastifyReply) : Promise<void> {
-    const { id } = request.params as ITodoGetParams;
-    const { limit = 10, offset = 0 } = request.query as any;
+    const { id } = request.params as ITodoRouteParams;
+    const { limit = 10, offset = 0 } = request.query as ITodoQueryParams;
+    
     try {
       if (id) {
         const todo = await this.getTodoUseCase.execute(id);
@@ -28,14 +33,13 @@ export class TodoController implements ITodoController {
     } catch (error) {
       if (error instanceof NotFoundError) {
         reply.status(404).send({
+          statusCode: 404,
+          error: "Not Found",
           message: `Item ${id} not found`,
-        });
-      } else if (error instanceof ValidationError) {
-        reply.status(400).send({
-          message: `Validation Error: ${error.message}`,
         });
       } else {
         reply.status(500).send({
+          statusCode: 500,
           message: "Internal Server Error",
         });
       }
@@ -43,6 +47,72 @@ export class TodoController implements ITodoController {
   }
 
   async createTodo(request: FastifyRequest, reply: FastifyReply) : Promise<void> {
-    throw new Error("Method not implemented.");
+    const { description, completed } = request.body as any;
+    
+    try {
+      const todo = await this.createTodoUseCase.execute({ description, completed });
+      reply.status(201).send(todo);
+    } catch (error) {
+      if (error instanceof EntityAlreadyExistsError) {
+        reply.status(409).send({
+          statusCode: 409,
+          error: "Conflict",
+          message: error.message,
+        });
+      } else {
+        reply.status(500).send({
+          statusCode: 500,
+          error: "Internal Server Error",
+        });
+      }
+    }
+  }
+
+  async updateTodo(request: FastifyRequest, reply: FastifyReply) : Promise<void> {
+    const { id } = request.params as ITodoRouteParams;
+    const { description, completed } = request.body as any;
+
+    try {
+      const todo = await this.updateTodoUseCase.execute(
+        id as number, 
+        { description, completed },
+      );
+      reply.status(200).send(todo);
+    } catch (error) {
+      if (error instanceof NotFoundError) {
+        reply.status(404).send({
+          statusCode: 404,
+          error: "Not Found",
+          message: `Item ${id} not found`,
+        });
+      } else {
+        reply.status(500).send({
+          statusCode: 500,
+          error: "Internal Server Error",
+        });
+      }
+    }
+  }
+
+  async deleteTodo(request: FastifyRequest, reply: FastifyReply) : Promise<void> {
+    const { id } = request.params as ITodoRouteParams;
+
+    try {
+      const todo = await this.deleteTodoUseCase.execute(id as number);
+      reply.status(200).send(todo);
+    } catch (error) {
+      if (error instanceof NotFoundError) {
+        reply.status(404).send({
+          statusCode: 404,
+          error: "Not Found",
+          message: `Item ${id} not found`,
+        });
+      } else {
+        reply.status(500).send({
+          statusCode: 500,
+          error: "Internal Server Error",
+        });
+      }
+    }
   }
 }
