51d1e0d07a2a374403aaba259ad72e565194e73a
Alessandro Bocci <a.bocci@doveconviene.com>
2023-08-18 23:03:01 +0200

    feat: create all crud queries

--- src/infrastructure/database/PostgresTodoDataAccess.ts
+++ src/infrastructure/database/PostgresTodoDataAccess.ts
@@ -1,36 +1,90 @@
 
 import { PoolClient } from "pg";
-import Todo from "../../domain/models/Todo";
+import { ICreateTodo, ITodo } from "../../domain/models/ITodo";
 import ITodoRepository from "../../domain/repositories/ITodoRepository";
 
 export default class PostgresTodoDataAccess implements ITodoRepository {
     constructor(private readonly client: PoolClient){}
 
-    create(todo: Todo): Promise<Todo> {
-        throw new Error("Method not implemented.");
+    async create(todo: ICreateTodo): Promise<ITodo> {    
+        const query = `
+            INSERT INTO todos (description, completed)
+            VALUES ($1, $2)
+            RETURNING *;
+        `;
+        const values = [todo.description, todo.completed];
+
+        const { rows } = await this.client.query<ITodo>(query, values);
+        return rows[0];
     }
-    async getAll(limit: number, offset: number): Promise<Todo[]> {
-        const { rows } = await this.client.query<Todo>(
-            "SELECT * FROM todos LIMIT $1 OFFSET $2",
-            [limit, offset]
-        );
+
+    async getAll(limit: number, offset: number): Promise<ITodo[]> {
+        const query =  "SELECT * FROM todos LIMIT $1 OFFSET $2";
+        const values = [limit, offset];
+
+        const { rows } = await this.client.query<ITodo>(query, values);
         return rows;
     }
     
-    async getById(id: number): Promise<Todo | null> {
-        const { rows } = await this.client.query<Todo>(
-            "SELECT * FROM todos WHERE id=$1",
-            [id],
-        );
+    async getById(id: number): Promise<ITodo | null> {
+        const query =  "SELECT * FROM todos WHERE id=$1";
+        const values = [id];
+
+        const { rows } = await this.client.query<ITodo>(query, values);
         if (!rows) {
             return null;
         }
         return rows[0];
     }
-    updateById(id: number): Promise<Todo> {
-        throw new Error("Method not implemented.");
+
+    async getByDescription(description: string) : Promise<ITodo | null> {
+        const query =  "SELECT * FROM todos WHERE description=$1";
+        const values = [description];
+
+        const { rows } = await this.client.query<ITodo>(query, values);
+        if (!rows) {
+            return null;
+        }
+        return rows[0];
     }
-    deleteById(id: number): Promise<Todo> {
-        throw new Error("Method not implemented.");
+
+    async updateById(id: number, updates: Partial<ITodo>): Promise<ITodo | null> {
+        const fields = [];
+        const values: (string | boolean | number)[] = [id];
+
+        for (const [field, value] of Object.entries(updates)) {
+            if (value !== undefined) {
+                fields.push(`${field} = $${fields.length + 2}`);
+                values.push(value);
+            }
+        }
+
+        const query = `
+            UPDATE todos
+            SET ${fields.join(', ')}
+            WHERE id = $1
+            RETURNING *;
+        `;
+
+        const { rows } = await this.client.query<ITodo>(query, values);
+        if (!rows.length) {
+            return null;
+        }
+        return rows[0];
+    }
+
+    async deleteById(id: number): Promise<ITodo | null> {
+        const query = `
+            DELETE FROM todos
+            WHERE id = $1
+            RETURNING *;
+        `;
+        const values = [id];
+
+        const { rows } = await this.client.query<ITodo>(query, values);
+        if (!rows.length) {
+            return null;
+        }
+        return rows[0];
     }
 }
